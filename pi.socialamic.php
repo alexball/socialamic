3768c744d739c27056a81d58e21535cf01032d09
Alex Ball <alexelmoball@gmail.com>
2012-11-17 23:23:25 -0500

    first commit
    Signed-off-by: Alex Ball <alexelmoball@gmail.com>

---
+++ pi.socialamic.php
@@ -0,0 +1,22 @@
+<?php
+class Plugin_socialamic extends Plugin {
+
+	var $meta = array(
+		'name'		 => 'Social:amic',
+		'version'	 => '0.1',
+		'author'	 => 'Alex Ball',
+		'author_url' => 'http://alexball.tv'
+	);
+
+	public function tweet()
+	{
+		$size = $this->fetch_param('size', 'large');
+		$count = $this->fetch_param('count', 'none');
+
+		$output = '
+			<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="{{ url }}" data-text="{{ title }}" data-count="'.$count.'" data-size="'.$size.'">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		';
+		return $output;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Social:amic
+===========
+
+A basic plugin for Statamic, which currently adds Twitter's official Tweet button to your post or page.
